4b4e88fe14fa53f5561a50350d6a388a0e7fed90
Ross Carrothers <ross.carrothers@gmail.com>
2015-06-04 23:11:14 -0700

    End of lesson 1. Creates a list view with dummmy text and displays it

--- app/src/main/java/ca/pintsofwine/sunshine/MainActivityFragment.java
+++ app/src/main/java/ca/pintsofwine/sunshine/MainActivityFragment.java
@@ -1,10 +1,17 @@
 package ca.pintsofwine.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -12,12 +19,40 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private ArrayAdapter weatherAdapter;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] weatherData = new String[] {
+                "Today - Sunny 88/63",
+                "Tomorrow - Sunny 88/63",
+                "Saturday - Sunny 88/63",
+                "Sunday - Sunny 88/63",
+                "Monday - Sunny 88/63",
+                "Tuesday - Sunny 88/63",
+                "Wednesday - Sunny 88/63"
+        };
+
+        List<String> weatherDataList = new ArrayList<String>(Arrays.asList(weatherData));
+        weatherAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weatherDataList
+        );
+
+        //Goal is to bind weatherAdapter to the list view (which is in fragment_main.xml ID
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(weatherAdapter);
+
+        return rootView;
     }
 }
