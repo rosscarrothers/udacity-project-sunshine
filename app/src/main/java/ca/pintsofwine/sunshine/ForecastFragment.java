f37d034da9341c163e5c8c673de23388c8e4cc3a
Ross Carrothers <ross.carrothers@gmail.com>
2015-06-07 21:22:14 -0700

    End of lesson 2. Refresh menu item calls a weather API and populates the list view with real data

--- app/src/main/java/ca/pintsofwine/sunshine/ForecastFragment.java
+++ app/src/main/java/ca/pintsofwine/sunshine/ForecastFragment.java
@@ -3,10 +3,12 @@ package ca.pintsofwine.sunshine;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,11 +19,19 @@ import java.util.List;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ForecastFragment extends Fragment {
+
+    private static final String LOG_TAG = ForecastFragment.class.getSimpleName();
 
     private ArrayAdapter weatherAdapter;
 
-    public MainActivityFragment() {
+    public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -50,9 +60,33 @@ public class MainActivityFragment extends Fragment {
 
         //Goal is to bind weatherAdapter to the list view (which is in fragment_main.xml ID
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-
         listView.setAdapter(weatherAdapter);
 
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_refresh) {
+            FetchWeatherTask task = new FetchWeatherTask(this);
+            task.execute("98119");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void updateWeatherData(String[] weatherData) {
+        ArrayList<String> weatherDataList = new ArrayList<String>(Arrays.asList(weatherData));
+        weatherAdapter.clear();
+
+        for (String forecast : weatherDataList) {
+            weatherAdapter.add(forecast);
+        }
     }
 }
